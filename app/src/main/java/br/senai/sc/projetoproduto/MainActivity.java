0991fa8e3650623cf888cbba7e70da6dfab6836a
Douglas Rosa <57238934+douglasvrosa@users.noreply.github.com>
2020-10-18 12:05:35 -0300

    Initial commit

---
+++ app/src/main/java/br/senai/sc/projetoproduto/MainActivity.java
@@ -0,0 +1,105 @@
+package br.senai.sc.projetoproduto;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import br.senai.sc.projetoproduto.modelo.Produto;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final int REQUEST_CODE_NOVO_PRODUTO = 1;
+    private final int RESULT_CODE_NOVO_PRODUTO = 10;
+    private final int REQUEST_CODE_EDITAR_PRODUTO = 2;
+    private final int RESULT_CODE_PRODUTO_EDITADO = 11;
+    private final int RESULT_CODE_PRODUTO_EXCLUIR = 12;
+
+    private ListView listViewProdutos;
+    private ArrayAdapter<Produto> adapterProdutos;
+    private int id = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Produtos");
+
+        listViewProdutos = findViewById(R.id.listView_produtos);
+        ArrayList<Produto> produtos = new ArrayList<Produto>();
+
+        adapterProdutos = new ArrayAdapter<Produto>(MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                produtos);
+        listViewProdutos.setAdapter(adapterProdutos);
+
+
+        definirOnClickListenerListView();
+
+    }
+
+    private void definirOnClickListenerListView() {
+        listViewProdutos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Produto produtoClicado = adapterProdutos.getItem(position);
+                Intent intent = new Intent(MainActivity.this, CadastroProdutoActivity.class);
+                intent.putExtra("produtoEdicao", produtoClicado);
+                startActivityForResult(intent, REQUEST_CODE_EDITAR_PRODUTO);
+            }
+        });
+    }
+
+    public void onClickNovoProduto(View v) {
+        Intent intent = new Intent(MainActivity.this, CadastroProdutoActivity.class);
+        startActivityForResult(intent, REQUEST_CODE_NOVO_PRODUTO);
+    }
+
+    public void onClickDeletar(View v) {
+        
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_CODE_NOVO_PRODUTO && resultCode == RESULT_CODE_NOVO_PRODUTO) {
+            Produto produto = (Produto) data.getExtras().getSerializable("novoProduto");
+            produto.setId(++id);
+            this.adapterProdutos.add(produto);
+        }else if (requestCode == REQUEST_CODE_EDITAR_PRODUTO && resultCode == RESULT_CODE_PRODUTO_EDITADO) {
+            Produto produtoEditado = (Produto) data.getExtras().getSerializable("produtoEditado");
+            for (int i = 0; i < adapterProdutos.getCount(); i++) {
+                Produto produto = adapterProdutos.getItem(i);
+                if (produto.getId() == produtoEditado.getId()) {
+                    adapterProdutos.remove(produto);
+                    adapterProdutos.insert(produtoEditado, i);
+                    break;
+                }
+            }
+        }else if (resultCode == RESULT_CODE_PRODUTO_EXCLUIR) {
+            Produto produtoExcluir = (Produto) data.getExtras().getSerializable("produtoExcluir");
+            for (int i = 0; i < adapterProdutos.getCount(); i++) {
+                Produto produto = adapterProdutos.getItem(i);
+                if (produto.getId() == produtoExcluir.getId()) {
+                    adapterProdutos.remove(produto);
+                    break;
+                }
+            }
+        }
+
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
